0f8dc3a926415a5e2fa6d996e292bcb9945908b4
Daniel <lungu.daniel94@gmail.com>
2020-03-06 01:01:28 +0200

    Part-1 -> Change the port of the MongoDB container from 27017 to 27085

--- src/main/docker/mongodb/scripts/init_replicaset.js
+++ src/main/docker/mongodb/scripts/init_replicaset.js
@@ -4,8 +4,8 @@ if (status.errmsg === 'no replset config has been received') {
 }
 for (var i = 1; i <= param; i++) {
     if (i!==1)
-        rs.add(folder+"_statistics-mongodb-node_" + i + ":27017");
+        rs.add(folder+"_statistics-mongodb-node_" + i + ":27085");
 }
 cfg = rs.conf();
-cfg.members[0].host = folder+"_statistics-mongodb-node_1:27017";
+cfg.members[0].host = folder+"_statistics-mongodb-node_1:27085";
 rs.reconfig(cfg);
