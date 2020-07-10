2e4b42f7aeef9695b55d31bccd02e50000e8f0d3
Daniel <lungu.daniel94@gmail.com>
2020-07-10 23:46:15 +0300

    Part-2 -> create entity

---
+++ src/main/java/org/travelers/statistics/domain/User.java
@@ -0,0 +1,82 @@
+package org.travelers.statistics.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.travelers.statistics.config.Constants;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@org.springframework.data.mongodb.core.mapping.Document(collection = "user")
+@org.springframework.data.elasticsearch.annotations.Document(indexName = "user_statistics_details")
+public class User extends AbstractAuditingEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @org.springframework.data.elasticsearch.annotations.Field(type = FieldType.Keyword)
+    private String id;
+
+    @NotNull
+    @Pattern(regexp = Constants.LOGIN_REGEX)
+    @Size(min = 1, max = 50)
+    @Indexed
+    private String login;
+
+    @NotNull
+    private LocalDate date;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+            Objects.equals(login, user.login) &&
+            Objects.equals(date, user.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, login, date);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+            "id='" + id + '\'' +
+            ", login='" + login + '\'' +
+            ", date=" + date +
+            '}';
+    }
+}
